cd27b337d8930bbb5ca7a610058d7411813713cd
mckadedaines <mckadedaines1@gmail.com>
2024-08-06 23:26:20 -0700

    Image carousel added and working

--- components/imageComponents/ImageCarousel.js
+++ components/imageComponents/ImageCarousel.js
@@ -1,27 +1,86 @@
-import React from "react";
+"use client";
+import React, { useEffect, useState } from "react";
 import Box from "@mui/material/Box";
+import Image from "next/image";
+import { Typography } from "@mui/material";
+import Slider from "react-slick";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
 
 function ImageCarousel() {
-  return (
-    <Box>
-      <h1>Image Carousel</h1>
+  const [images, setImages] = useState([]);
 
-      <div className="carousel">
-        <div className="carousel__container">
-          <div className="carousel__item">
-            <img src="https://via.placeholder.com/150" alt="Placeholder" />
-          </div>
-          <div className="carousel__item">
-            <img src="https://via.placeholder.com/150" alt="Placeholder" />
-          </div>
-          <div className="carousel__item">
-            <img src="https://via.placeholder.com/150" alt="Placeholder" />
-          </div>
-          <div className="carousel__item">
-            <img src="https://via.placeholder.com/150" alt="Placeholder" />
-          </div>
-        </div>
-      </div>
+  useEffect(() => {
+    fetch("/data/animeCarousel.json")
+      .then((response) => {
+        if (!response.ok) {
+          throw new Error("Network response was not ok " + response.statusText);
+        }
+        return response.json();
+      })
+      .then((data) => setImages(data))
+      .catch((error) => console.error("Error fetching images:", error));
+  }, []);
+
+  const settings = {
+    dots: true,
+    infinite: true,
+    speed: 1000,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 7000,
+  };
+
+  return (
+    <Box display="flex" justifyContent="center" alignItems="center">
+      <Slider {...settings} style={{ width: "50vw" }}>
+        {images.map((image, index) => (
+          <Box
+            key={index}
+            position="relative"
+            width="100%"
+            paddingTop="56.25%" // Aspect ratio 16:9 for half the width
+            overflow="hidden"
+          >
+            <Box
+              position="absolute"
+              top={0}
+              left={0}
+              width="100%"
+              height="100%"
+            >
+              <Image
+                src={image.src}
+                alt={image.title}
+                layout="fill"
+                objectFit="contain"
+                style={{ borderRadius: "10px" }}
+              />
+              <Box
+                position="absolute"
+                bottom={0}
+                left={0}
+                width="100%"
+                padding={2}
+                sx={{
+                  background:
+                    "linear-gradient(180deg, rgba(0,0,0,0) 0%, rgba(0,0,0,0.5) 30%, rgba(0,0,0,0.7) 70%)",
+                  borderBottomLeftRadius: "10px",
+                  borderBottomRightRadius: "10px",
+                }}
+              >
+                <Typography variant="h4" color="white" align="left">
+                  {image.title}
+                </Typography>
+                <Typography variant="body1" color="white" align="left">
+                  {image.description}
+                </Typography>
+              </Box>
+            </Box>
+          </Box>
+        ))}
+      </Slider>
     </Box>
   );
 }
