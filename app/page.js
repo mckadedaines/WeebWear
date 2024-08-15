4a8dc0109fa4dec45d323003811bfa08d7d181ac
mckadedaines <mckadedaines1@gmail.com>
2024-08-15 00:04:19 -0700

    Magic UI added, Meteors added to landing page. New potential hero images have been added

--- app/page.js
+++ app/page.js
@@ -3,14 +3,18 @@ import Header from "../components/common/Header";
 import ImageCarousel from "@/components/imageComponents/ImageCarousel";
 import ShirtGrid from "@/components/imageComponents/ShirtGrid";
 import PantsGrid from "@/components/imageComponents/PantsGrid";
+import Meteors from "@/components/magicui/meteors";
 
 export default function Home() {
   return (
-    <Box className="bg-black">
-      <Header />
-      <ImageCarousel />
-      <ShirtGrid />
-      <PantsGrid />
+    <Box className="relative bg-black">
+      <Meteors />
+      <Box className="relative z-10">
+        <Header />
+        <ImageCarousel />
+        <ShirtGrid />
+        <PantsGrid />
+      </Box>
     </Box>
   );
 }
